478e15ec395034ec174c9e2ed4f16df298290bfc
wy <381171634@qq.com>
2020-12-09 21:20:43 +0800

    1.增加低功耗文件

---
+++ app/lowPower.c
@@ -0,0 +1,57 @@
+#include "rtc.h"
+#include "lowPower.h"
+#include "gpio.h"
+#include "usart.h"
+
+void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc)
+{
+    HAL_NVIC_SystemReset();
+}
+
+void enter_lowPwr()
+{
+    RTC_AlarmTypeDef alarm;
+    RTC_TimeTypeDef time;
+    memset((void *)&alarm, 0, sizeof(alarm));
+    //除了控制GPRS模块的DCDC使能，其余引脚全配置成浮空输入
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_All & ~(GPIO_PIN_13 | GPIO_PIN_14));
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_All & ~(GPIO_PIN_15));
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_All);
+
+    HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
+    //time.Seconds += 5;
+    // if (time.Seconds > 59)
+    // {
+    //     time.Minutes += 1;
+    //     time.Seconds = time.Seconds % 60;
+    //     if (time.Minutes > 59)
+    //     {
+    //         time.Hours += 1;
+    //         time.Minutes = 0;
+    //         if (time.Hours > 23)
+    //             time.Hours = 0;
+    //     }
+    // }
+    time.Seconds = 0;
+    time.Minutes += 5;
+    if(time.Minutes > 59)
+    {
+        time.Hours += 1;
+        time.Minutes = 0;
+        if (time.Hours > 23)
+            time.Hours = 0;
+    }
+
+    alarm.AlarmTime.Hours = time.Hours;
+    alarm.AlarmTime.Minutes = time.Minutes;
+    alarm.AlarmTime.Seconds = time.Seconds;
+
+    HAL_RTC_SetAlarm_IT(&hrtc,&alarm,RTC_FORMAT_BIN);
+    
+    SysTick->CTRL = 0x00;//关闭定时器
+    SysTick->VAL = 0x00;//清空val,清空定时器
+    HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON,PWR_STOPENTRY_WFI);
+
+    //SET_BIT(PWR->CR, PWR_CR_CWUF);
+    //HAL_PWR_EnterSTANDBYMode();
+}
---
+++ app/lowPower.h
@@ -0,0 +1,9 @@
+#ifndef _LOWPOWER_H
+#define _LOWPOWER_H
+
+#include "common.h"
+
+void enter_lowPwr();
+
+
+#endif
